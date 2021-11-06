6ad7a4bec0eb7f4a1990c194535b838199efa3dc
Lightfooted <vesselofbalance@hotmail.com>
2021-11-06 19:46:55 -0400

    finished mod

--- src/components/Gallery/__tests__/index.test.js
+++ src/components/Gallery/__tests__/index.test.js
@@ -3,8 +3,8 @@ import React from 'react'
 import { render, cleanup } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 import Gallery from '..'
-
 const portrait = { name: "portraits", description: "Portraits of people in my life" };
+
 
 afterEach(cleanup)
 
@@ -20,7 +20,7 @@ describe('Gallery component', () => {
   })
 });
 
-it('renders', () => {
+it('displays text', () => {
   const { getByTestId } = render(<Gallery currentCategory={portrait} />)
   expect(getByTestId('h1tag')).toHaveTextContent('Portraits')
 })
